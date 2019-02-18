be7fec27d2557e1e9a5c7cbb702a1d71ad1c5b2d
Nabil Fares <nabilfares@Nabils-MacBook-Pro.local>
2019-02-18 19:33:12 +0300

    Nabil

--- iterators.js
+++ iterators.js
@@ -1,60 +1,62 @@
 /*************************************
-* logger(array)
-*
-* - Accepts an array
-* - Logs every element of the array
-************************************/
+ * logger(array)
+ *
+ * - Accepts an array
+ * - Logs every element of the array
+ ************************************/
 const logger = function(array) {
   // Your code here
+  array.forEach(item => console.log(item));
 };
 
-
 /*************************************
-* toCelsius(temperatures)
-*
-* - Accepts an array of temperatures
-*   in degrees Fahrenheit
-* - Returns an array of temperatures
-*   in degrees Celsius
-*
-* The conversion is:
-*   C = (F - 32) * (5/9)
-************************************/
+ * toCelsius(temperatEachures)
+ *
+ * - Accepts an array of temperatures
+ *   in degrees Fahrenheit
+ * - Returns an array of temperatures
+ *   in degrees Celsius
+ *
+ * The conversion is:
+ *   C = (F - 32) * (5/9)
+ ************************************/
 const toCelsius = function(temperatures) {
   // Your code here
-}
-
+  let celsius = temperatures.map(temp => (temp - 32) * (5 / 9));
+  return celsius;
+};
 
 /**************************************
-* hottestDays(temperatures, threshhold)
-*
-* - Accepts an array of temperatures
-* - Accepts a threshhold temperature
-* - Returns an array of temperatures
-*   that exceed the threshhold
-***************************************/
+ * hottestDays(temperatures, threshhold)
+ *
+ * - Accepts an array of temperatures
+ * - Accepts a threshhold temperature
+ * - Returns an array of temperatures
+ *   that exceed the threshhold
+ ***************************************/
 const hottestDays = function(temperatures, threshhold) {
   // Your code here
-}
-
+  let exceed = temperatures.filter(temp => temp > threshhold);
+  return exceed;
+};
 
 /******************************************
-* logHottestDays(temperatures, threshhold)
-*
-* - Accepts an array of temperatures
-*   IN DEGREES FAHRENHEINT
-* - Accepts a threshhold temperature
-*   IN DEGREES FAHRENHEINT
-* - Logs temperatures that exceed the
-*   threshhold to the console
-*   IN DEGREES CELSIUS
-*
-* hint: you can combine
-*       all previous functions
-*******************************************/
+ * logHottestDays(temperatures, threshhold)
+ *
+ * - Accepts an array of temperatures
+ *   IN DEGREES FAHRENHEINT
+ * - Accepts a threshhold temperature
+ *   IN DEGREES FAHRENHEINT
+ * - Logs temperatures that exceed the
+ *   threshhold to the console
+ *   IN DEGREES CELSIUS
+ *
+ * hint: you can combine
+ *       all previous functions
+ *******************************************/
 const logHottestDays = function(temperatures, threshhold) {
   // Your code here
-}
+  logger(toCelsius(hottestDays(temperatures, threshhold)));
+};
 
-
-export {logger, toCelsius, hottestDays, logHottestDays}
+export { logger, toCelsius, hottestDays, logHottestDays };
